4b243e6bf5d77f32bdb5ed5d7b9cb47a1d975e92
Joe Freedman <joseph.a.freedman@gmail.com>
2023-02-10 12:05:47 -0500

    Use regex in experian script

--- scripts/experian-redirect.js
+++ scripts/experian-redirect.js
@@ -1,11 +1,11 @@
 // This file is formatted for use with this add-on: https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/
 // ==UserScript==
 // @name     ExperianRedirect
-// @version  1.0
+// @version  2.1
 // @grant    none
-// @include        https://usa.experian.com/member/preload
+// @include        /https://usa.experian.com/.*member/preload/
 // ==/UserScript==
 
 // After login, Experian directs a non-paid user to a page asking to upgrade to a paid account
-// This script automatically redirects the user after login from immediately to the user's dashboard
+// This script automatically redirects the user after login immediately to the user's dashboard
 window.location.replace ("https://usa.experian.com/member/dashboard");
